34ef0f8308543b339e69af68ccc8e8a6554462dc
aa-rju <mahataaarju@gmail.com>
2024-11-10 14:56:35 +0545

    initial commit

--- app.js
+++ app.js
@@ -12,47 +12,47 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Nodemailer transporter setup
-const transporter = nodemailer.createTransport({
-  service: "Gmail", // Replace with your email service
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "Gmail", // Replace with your email service
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html")); // Adjust the path if needed
 });
 
-app.post("/contact", (req, res) => {
-  const { name, email, subject, message } = req.body;
+// app.post("/contact", (req, res) => {
+//   const { name, email, subject, message } = req.body;
 
-  const mailOptions = {
-    from: email,
-    to: process.env.MAIL_USER, // Your email to receive the messages
-    subject: subject || `New Contact Form Message from ${name}`,
-    text: `You have received a new message from ${name} (${email}):\n\n${message}`,
-  };
+//   const mailOptions = {
+//     from: email,
+//     to: process.env.MAIL_USER, // Your email to receive the messages
+//     subject: subject || `New Contact Form Message from ${name}`,
+//     text: `You have received a new message from ${name} (${email}):\n\n${message}`,
+//   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email:", error);
-      res.status(500).json({ message: "Failed to send email" });
-    } else {
-      console.log("Email sent:", info.response);
-      res.status(200).send(`
-      <html>
-        <head><title>Success</title></head>
-        <body>
-          <h1>Your message has been sent successfully!</h1>
-          <p>Thank you for contacting😊.</p>
-        </body>
-      </html>`);
-      // .json({ message: "Your message has been sent successfully!" });
-      // toast.success('Your message has been sent successfully!');
-    }
-  });
-});
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.error("Error sending email:", error);
+//       res.status(500).json({ message: "Failed to send email" });
+//     } else {
+//       console.log("Email sent:", info.response);
+//       res.status(200).send(`
+//       <html>
+//         <head><title>Success</title></head>
+//         <body>
+//           <h1>Your message has been sent successfully!</h1>
+//           <p>Thank you for contacting😊.</p>
+//         </body>
+//       </html>`);
+//       // .json({ message: "Your message has been sent successfully!" });
+//       // toast.success('Your message has been sent successfully!');
+//     }
+//   });
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
